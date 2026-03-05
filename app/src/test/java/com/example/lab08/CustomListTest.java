2a5191a1cb968a3fe9c784ef07f8edd2ce818265
David Zhao <144359888+davidpyzhao@users.noreply.github.com>
2026-03-04 17:38:42 -0700

    broken test

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -2,13 +2,27 @@ package com.example.lab08;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+
 public class CustomListTest {
+    public CustomList MockCityList() {
+        return new CustomList();
+    }
     @Test
     public void testHasCity() {
         CustomList list = new CustomList();
         City calgary = new City("Calgary", "AB");
         list.addCity(calgary);
-// This will fail initially because hasCity() doesn't exist
+        // This will fail initially because hasCity() doesn't exist
         assertTrue(list.hasCity(calgary));
+    }
+    
+    @Test
+    public void addCityTest() {
+        CustomList list = MockCityList();
+        // This will fail initially because getCount() doesn't exist
+        int listSize = list.getCount();
+        list.addCity(new City("Estevan", "SK"));
+        assertEquals(listSize + 1, list.getCount());
     }
 }
